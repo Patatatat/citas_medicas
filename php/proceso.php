7e67b4d7874d7b7c72c8872ba570c9231df9c5fb
Andres <100623904+Patatatat@users.noreply.github.com>
2025-11-12 16:52:16 -0700

    proceso con las variables de html

--- php/proceso.php
+++ php/proceso.php
@@ -1,16 +1,17 @@
 <?php 
 include("conexion.php"); //conexion de proceso.php a la base de datos
-$nomUsuario = $_POST['usuario'];
-$edadUsuario = $_POST['Edad_in'];
-$correoUsuario = $_POST['Correo_in'];
-$contraUsuario = $_POST['Contrasena_in']; #Variables del index.html
+$nomPaciente = $_POST['paciente'];
+$nomMedico = $_POST['medico'];
+$fecha = $_POST['fecha'];
+$hora = $_POST['hora']; #Variables del index.html
+$especialidad = $_POST['profecion'];
 
 //conexion en base de datos, formulario html y proceso
-if ($nomUsuario =="" || $edadUsuario == "" || $correoUsuario == "" || $contraUsuario == "") {
+if ($nomPaciente =="" || $nomMedico == "" || $fecha == "" || $hora == "" || $especialidad == "") {
 	echo "Faltan llenar campos";
 }
 else{
-	$sql = 'INSERT INTO usuario(nombreUsuario, edadUsuario, correoUsuario, passUsuario) VALUES ("'.$nomUsuario.'","'.$edadUsuario.'","'.$correoUsuario.'","'.$contraUsuario.'")';
+	$sql = 'INSERT INTO usuario(nombreUsuario, edadUsuario, correoUsuario, passUsuario) VALUES ("'.$nomPaciente.'","'.$nomMedico.'","'.$fecha.'","'.$hora.'","'.$especialidad.'")';
 
 	//Ejecutar SQL de insercion a la BD
 	$resultado = DataBase::ejecutar_sql($sql);
