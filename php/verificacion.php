3053ca250f7b167b61190ad754eb7cf0b3d92415
Andres <100623904+Patatatat@users.noreply.github.com>
2025-12-05 15:00:57 -0700

    verificacion.php terminada

--- php/verificacion.php
+++ php/verificacion.php
@@ -6,10 +6,13 @@ if($email != "" || $contra != ""){
   $sql = "SELECT * FROM usuario WHERE Correo = '$email' AND Contrasena = '$contra'";
   $resultado = Database::get_rows($sql);
   $verificacion = mysqli_fetch_assoc($resultado);
-  if($verificacion == 1) {
+  if(isset($verificacion["Correo"]) == $email && isset($verificacion["Contrasena"]) == $contra) {
     ?>
     <a href="../templates/menu.php"><button>Ingresar</button></a>
   <?php }  
+  else{ ?>
+  <a href="../templates/index.php"><button>Regresar</button></a>
+<?php }
 }
 else{ ?>
   <a href="../templates/index.php"><button>Regresar</button></a>
